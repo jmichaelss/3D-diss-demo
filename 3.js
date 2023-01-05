f12dd25ccd605adca51d09100ff9fd07fb9382bd
jmichaelss <njoelmichael1999@gmail.com>
2023-01-05 14:11:14 +0000

    unused imports

--- 3.js
+++ 3.js
@@ -1,7 +1,6 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js";
 import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js";
 import { OBJLoader } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/loaders/OBJLoader.js";
-import { MTLLoader } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/loaders/MTLLoader.js";
 
 // Declare variables for the scene, camera, and renderer
 var scene, camera, renderer;
